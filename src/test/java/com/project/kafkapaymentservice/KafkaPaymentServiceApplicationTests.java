5db434cf2d318f13f5f4ede7077f998d36367cae
Ulugbek Ishmatov <ishmatovulugbek@gmail.com>
2025-12-24 18:27:53 +0300

    chore: update gitignore for kafka and docker

---
+++ src/test/java/com/project/kafkapaymentservice/KafkaPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kafkapaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
